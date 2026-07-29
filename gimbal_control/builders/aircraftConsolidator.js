862faadb1dce277901aed6faf9d8efc8c9d9e187
Peter <ballardpet@gmail.com>
2026-07-29 15:48:29 -0500

    Created aircraftConsolidator and added some notes in there for tomorrow.

---
+++ gimbal_control/builders/aircraftConsolidator.js
@@ -0,0 +1,11 @@
+/**
+ * This will be the file to consolidate aircraft JSON files
+ * 
+ * For now we'll read from the ADSB and P5 files, but eventually we'll add other tracking types as required
+ * 
+ * Let's track: Latitude, Longitude, Altitude, id (hexid for adsb, callsign for p5, whatever for other tracking types), tracking type
+ * 
+ * Adding tracking type is important so we can have a filter on the map eventually
+ * 
+ * Reasearch this once ready for the map: https://leafletjs.com/
+ */
