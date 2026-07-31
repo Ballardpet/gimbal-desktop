ee2131f91d9bfcf13cabf680e3ac28816d978220
Peter <ballardpet@gmail.com>
2026-07-31 09:08:54 -0500

    Map is looking good! Next step is to move it over to the gps file and try to start adding aircraft.

--- src/components/map_test.jsx
+++ src/components/map_test.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-import world from "../data/world.json";
+import world from "../data/crisp_world.json";
+// import world from "../data/world.json";
 
 //import ne_50m_admin_0_countries.json
 
@@ -11,18 +12,28 @@ import world from "../data/world.json";
 // maybe pm tiles with leaflet? https://docs.protomaps.com/pmtiles/leaflet
 
 // This map might be what I'm looking for: https://www.naturalearthdata.com/
+// Convert it to geoJSON: https://mapshaper.org/
 
 // probably do actually start with leaflet. seems simpler for a first prototype 
     // I have a geojson file downloaded now. gotta figure out how to use thiss
 
+// Using the fancy map makes the program slow to load. Maybe do a different map?
 
 export default function Map_Test() {
 
     const mapRef = useRef(null)
 
     useEffect(() => {
-        const map = L.map(mapRef.current).setView([20, 0], 2);
-        L.geoJSON(world).addTo(map);
+        const map = L.map(mapRef.current).setView([30.4719, -86.5422], 6);
+        L.geoJSON(world, {
+            style: {
+                color: "#666666",
+                weight: 0.5,
+                //fillColor: "#d9d9d9",
+                fillColor: "#d9d9d9",
+                fillOpacity: 1
+            }
+        }).addTo(map);
         return () => {
             map.remove();
         };
