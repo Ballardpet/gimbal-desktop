e5f77586e8faf4fcc9c74a6e500ba12f82898f6c
Peter <ballardpet@gmail.com>
2026-07-31 08:39:46 -0500

    Aw HELL yeah we have a basic map. Gonna make it look a bit better before trying to add aircraft. Yippee!

--- src/components/map_test.jsx
+++ src/components/map_test.jsx
@@ -1,9 +1,36 @@
 import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import world from "../data/world.json";
+
+//import ne_50m_admin_0_countries.json
+
+// maybe actually use protomaps and maplibre
+
+// maybe pm tiles with leaflet? https://docs.protomaps.com/pmtiles/leaflet
+
+// This map might be what I'm looking for: https://www.naturalearthdata.com/
+
+// probably do actually start with leaflet. seems simpler for a first prototype 
+    // I have a geojson file downloaded now. gotta figure out how to use thiss
+
 
 export default function Map_Test() {
+
+    const mapRef = useRef(null)
+
+    useEffect(() => {
+        const map = L.map(mapRef.current).setView([20, 0], 2);
+        L.geoJSON(world).addTo(map);
+        return () => {
+            map.remove();
+        };
+    }, []);
+
     return (
         <section>
-            <div>Put map stuff here</div>
+            <div ref={mapRef} className="map"></div>
         </section>
     )
 }
