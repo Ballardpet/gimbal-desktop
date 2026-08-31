0bcce37556bc148da8c5cab9d7c82b4a4b87c001
Peter <ballardpet@gmail.com>
2026-08-31 09:17:38 -0500

    Everything looks good. Saving working code. Next step is to update readme before sending out

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// Maybe don't even need the title. Probably remove eventually to make room for antenna control
+// Maybe make a little gimbal graphic
 export default function Title() {
     return (
         <h1 className='center_elements'>Gimbal Controller</h1>
